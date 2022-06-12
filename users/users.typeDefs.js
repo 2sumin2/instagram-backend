ae57ad93ffc24c38e78407d82df5f21bc6d7f15f
sumin <lsm001005@naver.com>
2022-06-12 18:07:07 +0900

    Update

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -3,6 +3,7 @@ import { gql } from "apollo-server";
 export default gql`
     type User{
         id: Int!
+        email: String!
         username: String!
         statement: String!
         createAt: String!
