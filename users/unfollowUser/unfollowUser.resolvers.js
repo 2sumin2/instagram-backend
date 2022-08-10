6d917665add1a12f0456d9e04fc7038700105db4
sumin <lsm001005@naver.com>
2022-08-10 23:44:48 +0900

    Unfollow

---
+++ users/unfollowUser/unfollowUser.resolvers.js
@@ -0,0 +1,33 @@
+import client from "../../client";
+
+export default {
+    Mutation: {
+        unfollowUser: (async (_, { username, id }) => {
+            const ok = await client.user.findUnique({
+                where: { username },
+            });
+            if (!ok) {
+                return {
+                    ok: false,
+                    error: "Can't unfollow user.",
+                };
+            }
+            await client.user.update({
+                where: {
+                    id
+                },
+                data: {
+                    following: {
+                        disconnect: {
+                            username,
+                        },
+                    },
+                },
+            });
+            return {
+                ok: true,
+            };
+        }
+        ),
+    },
+};
